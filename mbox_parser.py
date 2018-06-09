1c79b76e37b981bc8f6bd02beb012da86a632e2e
SabinaP3152 <39749128+SabinaP3152@users.noreply.github.com>
2018-06-09 18:10:42 -0400

    Update and rename README.md to mbox_parser.py

---
+++ mbox_parser.py
@@ -0,0 +1,6 @@
+import mailbox
+import csv
+
+writer = csv.writer(open("clean_mail.csv", "wb"))
+for message in mailbox.mbox('your_mbox_name'):
+    writer.writerow([message['subject'], message['from'], message['date']])https://github.com/SabinaP3152
